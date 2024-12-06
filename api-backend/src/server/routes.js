0b956b1a3a58dbe65a020a06d6169e3a627cd351
sirWerq <rifqx1904@gmail.com>
2024-12-06 08:42:10 +0700

    add at dev

---
+++ api-backend/src/server/routes.js
@@ -0,0 +1,84 @@
+const {
+    articlesApi,
+    detailsArticleApi,
+    bannersApi,
+    recyclesApi,
+    registerApi,
+    loginApi,
+    resetPasswordApi,
+    editProfileApi,
+    detailsRecycleApi,
+} = require('./handler');
+const verifyJWT = require('../middleware');
+
+const routes = [
+    {
+        method: 'GET',
+        path: '/api/articles',
+        handler: articlesApi,
+        options: {
+            pre: [{ method: verifyJWT }],
+        },
+    },
+    {
+        method: 'GET',
+        path: '/api/articles/{id}',
+        handler: detailsArticleApi,
+        options: {
+            pre: [{ method: verifyJWT }],
+        },
+    },
+    {
+        method: 'GET',
+        path: '/api/banners',
+        handler: bannersApi,
+        options: {
+            pre: [{ method: verifyJWT }],
+        },
+    },
+    {
+        method: 'GET',
+        path: '/api/recycles/{id}',
+        handler: detailsRecycleApi,
+        options: {
+            pre: [{ method: verifyJWT }],
+        },
+    },
+    {
+        method: 'POST',
+        path: '/predict',
+        handler: recyclesApi,
+        options: {
+            payload: {
+                allow: 'multipart/form-data',
+                multipart: true,
+            },
+            pre: [{ method: verifyJWT }],
+        },
+    },
+    {
+        method: 'POST',
+        path: '/register',
+        handler: registerApi,
+    },
+    {
+        method: 'POST',
+        path: '/login',
+        handler: loginApi,
+    },
+    {
+        method: 'POST',
+        path: '/reset-password',
+        handler: resetPasswordApi,
+    },
+    {
+        method: 'POST',
+        path: '/edit-profile',
+        handler: editProfileApi,
+        options: {
+            pre: [{ method: verifyJWT }],
+        },
+    },
+];
+
+module.exports = routes;
